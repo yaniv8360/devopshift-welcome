a32ffc68ef22a078c917e9b5baf6eb0b66c7ee92
Your Name <you@example.com>
2025-02-05 10:05:31 +0000

    lab102p2

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    time = {
-      source  = "hashicorp/time"
-      version = "0.12.1"  # Adjust to the latest version if necessary
-    }
-  }
-}
-resource "time_sleep" "wait_for_ip" {
-  create_duration = "10s"  # Wait for 10 seconds
-}
-
 provider "aws" {
   region = var.region
 }
@@ -18,10 +6,12 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "xyz" {
-    default = "us-east-1"
-  
+
+# Mocked IP var
+variable "emptyip" {
+    default = ""
 }
+
 resource "aws_security_group" "sg" {
   ingress {
     from_port   = 22
@@ -37,6 +27,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_instance" "vm" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI in us-east-1
   instance_type = "t2.micro"
@@ -47,14 +38,26 @@ resource "aws_instance" "vm" {
     Name = "YanivRotics-vm"
   }
 }
+
+
+
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
-  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
   description = "Public IP address of the VM"
+  depends_on = [ null_resource.check_public_ip ]
 }
 
-resource "null_resource" "run_script" {
+resource "null_resource" "check_public_ip" {
   provisioner "local-exec" {
-    command = "echo 'Running a script after provisioning.'"
+    command = <<EOT
+      if [ -z "${aws_instance.vm.public_ip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+        else
+        echo "We got the IP! ${aws_instance.vm.public_ip}"
+      fi
+    EOT
   }
+
+  depends_on = [aws_instance.vm]
 }
