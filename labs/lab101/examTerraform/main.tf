25b56734a0ef5ca4a38b38eb39a36eb5bfd5d2e8
yaniv <yaniv.rotics@live.biu.ac.il>
2025-02-07 01:03:20 +0200

    task 02

--- labs/lab101/examTerraform/main.tf
+++ labs/lab101/examTerraform/main.tf
@@ -1,3 +1,6 @@
+variable "region" {
+  default = "us-east-1"
+}
 # Create VPC only if enabled
 resource "aws_vpc" "custom_vpc" {
  count = 1
@@ -65,4 +68,74 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private.id
+}
+
+# data "aws_region" "current" {}
+
+# output "aws_region" {
+#   value = data.aws_region.current.name
+# }
+
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-sg"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.custom_vpc[0].id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "instance-sg"
+  }
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "YanivRoticsKeyPair"  # Name of the key pair in AWS
+  public_key = file("id_YanivRoticsRsa.pub")  # Path to your public key file
+}
+
+# # Output the key pair name
+# output "key_pair_name" {
+#   value = aws_key_pair.my_key_pair.key_name
+# }
+
+# # Output the key pair ID
+# output "key_pair_id" {
+#   value = aws_key_pair.my_key_pair.id
+# }
+resource "aws_instance" "ubuntu_instance" {
+  ami                    = "ami-0e1bed4f06a3b463d"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+  key_name               = "YanivRoticsKeyPair"
+
+  tags = {
+    Name = "YanivRoticsUbuntuMachine"
+  }
+}
+output "vm_public_ip" {
+  value       = aws_instance.ubuntu_instance.public_ip
+  description = "Public IP address of the VM"
 }
